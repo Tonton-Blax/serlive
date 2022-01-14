391822808332e8f352a75cf83e91d0e95f8bb7b3
Tonton-Blax <redval@gmail.com>
2022-01-14 21:09:58 +0100

    first test

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,19 @@
 <script>
+	//https://www.youtube.com/watch?v=QdStKZkNWm4
+	import Plyr from 'plyr';
 	export let name;
+	const player = new Plyr('#player');
+
 </script>
 
 <main>
 	<h1>Hello {name}!</h1>
 	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+
+	<div class="container">
+		<div id="player" data-plyr-provider="youtube" data-plyr-embed-id="QdStKZkNWm4"></div>
+	</div>
+	<!-- Plyr resources and browser polyfills are specified in the pen settings -->
 </main>
 
 <style>
